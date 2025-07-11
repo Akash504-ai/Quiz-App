f8c78425129c8a84d541b5ff2b61929c75fbb9f2
Akash504-ai <santraakash999@gmail.com>
2025-07-11 23:35:22 +0530

    Initial commit

---
+++ src/assets/data.js
@@ -0,0 +1,46 @@
+export const data = [
+  {
+    question: "What does CPU stand for?",
+    option1: "Central Processing Unit",
+    option2: "Computer Personal Unit",
+    option3: "Central Process Utility",
+    option4: "Control Program Unit",
+    ans: 1,
+  },
+
+  {
+    question: "Which of the following is a web browser?",
+    option1: "Linux",
+    option2: "Google Chrome",
+    option3: "GitHub",
+    option4: "Python",
+    ans: 2,
+  },
+
+  {
+    question: "What is the full form of HTML?",
+    option1: "Hyper Text Making Language",
+    option2: "High Transfer Markup Language",
+    option3: "Hyper Text Markup Language",
+    option4: "Hyperlink Text Main Language",
+    ans: 3,
+  },
+
+  {
+    question: "Which one is a programming language?",
+    option1: "HTTP",
+    option2: "RAM",
+    option3: "JavaScript",
+    option4: "WiFi",
+    ans: 3,
+  },
+
+  {
+    question: "What is the brain of the computer?",
+    option1: "Monitor",
+    option2: "Mouse",
+    option3: "CPU",
+    option4: "Hard Drive",
+    ans: 3,
+  }
+]
